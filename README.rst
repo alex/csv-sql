964cf278ef94093f1d9f38709376f67c4478a220
Alex Gaynor <alex.gaynor@gmail.com>
2016-12-31 11:18:51 -0500

    Rewrite it all in rust (#3)
    * Rewrite it all in rust
    
    * inline this, it's very simple
    
    * Ignore whitespace when skipping lines
    
    * This is no longer rust specific
    
    * multiple tables, wowa!
    
    * cargo fmt
    
    * this fits on one line
    
    * simple completer, will add more words and column names later
    
    * autocomplete the column names too!

--- README.rst
+++ README.rst
@@ -5,16 +5,20 @@ Take a CSV file, query it with SQL. Magic!
 
 .. code-block:: console
 
-    $ python csvsql.py file.csv
+    $ cargo run file.csv
     Loaded 3162 rows into t(domain, base_domain, agency, sslv2)
     > SELECT COUNT(*) FROM t
-    +----------+
-    | count(*) |
     +----------+
     | 3162     |
     +----------+
 
 All your rows go into a table named ``t``. It's great!
 
-Error handling is bad, and the repl is super janky, no readline or anything
-pleasant yet.
+You can also specific multiple files:
+
+.. code-block:: console
+
+    $ cargo run file1.csv file2.csv
+    Loaded 12 rows into t1(some, schema)
+    Loaded 74 rows into t2(some, other, schema)
+    >
---
+++ src/main.rs
@@ -0,0 +1,156 @@
+extern crate csv;
+extern crate prettytable;
+extern crate regex;
+extern crate rustyline;
+extern crate sqlite3;
+
+use std::env;
+
+
+fn _normalize_col(col: &String) -> String {
+    let re = regex::Regex::new(r"\(.*?\)").unwrap();
+    return re.replace_all(col, "")
+        .to_lowercase()
+        .trim()
+        .replace(" ", "_")
+        .replace(".", "_")
+        .replace("?", "");
+}
+
+fn _create_table(db: &mut sqlite3::DatabaseConnection, table_name: &str, cols: &Vec<String>) {
+    let create_columns =
+        cols.iter().map(|c| format!("{} varchar", c)).collect::<Vec<String>>().join(", ");
+    db.exec(&format!("CREATE TABLE {} ({})", table_name, create_columns)).unwrap();
+}
+
+fn _insert_row(db: &mut sqlite3::DatabaseConnection,
+               table_name: &str,
+               row: Vec<String>,
+               cols: &Vec<String>) {
+    let placeholders = cols.iter()
+        .enumerate()
+        .map(|(idx, _)| format!("${}", idx + 1))
+        .collect::<Vec<String>>()
+        .join(", ");
+    let mut stmt = db.prepare(&format!("INSERT INTO {} VALUES ({})", table_name, placeholders))
+        .unwrap();
+
+    for (idx, value) in row.iter().enumerate() {
+        stmt.bind_text((idx + 1) as sqlite3::ParamIx, value).unwrap();
+    }
+    assert!(stmt.execute().step().unwrap().is_none());
+}
+
+fn _load_table_from_path(db: &mut sqlite3::DatabaseConnection,
+                         table_name: &str,
+                         path: String)
+                         -> Vec<String> {
+    let mut num_rows = 0;
+    let mut reader = csv::Reader::from_file(path).unwrap();
+
+    let normalized_cols = reader.headers().unwrap().iter().map(_normalize_col).collect();
+    _create_table(db, table_name, &normalized_cols);
+
+    for row in reader.decode() {
+        _insert_row(db, table_name, row.unwrap(), &normalized_cols);
+        num_rows += 1;
+    }
+
+    println!(
+        "Loaded {} rows into {}({})",
+        num_rows,
+        table_name,
+        normalized_cols.join(", "),
+    );
+    return normalized_cols;
+}
+
+fn _print_table(conn: &mut sqlite3::DatabaseConnection, line: &str) {
+    let mut table = prettytable::Table::new();
+    table.set_format(*prettytable::format::consts::FORMAT_NO_LINESEP_WITH_TITLE);
+    let mut stmt = match conn.prepare(&line) {
+        Ok(stmt) => stmt,
+        Err(e) => {
+            println!("{}", e.detail.unwrap());
+            return;
+        }
+    };
+    let mut results = stmt.execute();
+    while let Some(r) = results.step().unwrap() {
+        let mut row = prettytable::row::Row::new(vec![]);
+        for i in 0..r.column_count() {
+            row.add_cell(prettytable::cell::Cell::new(&r.column_text(i).unwrap_or("".to_string())));
+        }
+        table.add_row(row);
+    }
+    table.printstd();
+}
+
+struct SimpleWordCompleter {
+    words: Vec<String>,
+}
+
+
+static BREAK_CHARS: [char; 4] = [' ', '(', ')', ','];
+impl SimpleWordCompleter {
+    fn new(words: Vec<String>) -> SimpleWordCompleter {
+        return SimpleWordCompleter { words: words };
+    }
+}
+
+impl rustyline::completion::Completer for SimpleWordCompleter {
+    fn complete(&self, line: &str, pos: usize) -> rustyline::Result<(usize, Vec<String>)> {
+        let (start, word) =
+            rustyline::completion::extract_word(line, pos, &BREAK_CHARS.iter().cloned().collect());
+
+        let matches = self.words.iter().filter(|w| w.starts_with(word)).cloned().collect();
+        return Ok((start, matches));
+    }
+}
+
+fn main() {
+    let mut paths = env::args().skip(1);
+
+    let mut conn = sqlite3::DatabaseConnection::in_memory().unwrap();
+
+    let mut base_words = vec!["select", "from", "group", "by", "order", "count"]
+        .iter()
+        .map(|s| s.to_string())
+        .collect::<Vec<String>>();
+
+    if paths.len() == 1 {
+        let mut col_names = _load_table_from_path(&mut conn, "t", paths.next().unwrap());
+        base_words.append(&mut col_names);
+    } else {
+        for (idx, path) in paths.enumerate() {
+            let mut col_names = _load_table_from_path(&mut conn, &format!("t{}", idx + 1), path);
+            base_words.append(&mut col_names);
+        }
+    }
+
+    let completer = SimpleWordCompleter::new(base_words);
+    let mut rl = rustyline::Editor::new();
+    rl.set_completer(Some(completer));
+    loop {
+        match rl.readline("> ") {
+            Ok(line) => {
+                if line.trim().is_empty() {
+                    continue;
+                }
+                rl.add_history_entry(&line);
+                _print_table(&mut conn, &line);
+            }
+            Err(rustyline::error::ReadlineError::Interrupted) => {
+                println!("Interrupted");
+                continue;
+            }
+            Err(rustyline::error::ReadlineError::Eof) => {
+                break;
+            }
+            Err(err) => {
+                println!("Error: {}", err);
+                break;
+            }
+        }
+    }
+}
