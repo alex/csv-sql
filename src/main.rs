803eb0d460af89657f1ff5140fa12ebee37e4424
Alex Gaynor <alex.gaynor@gmail.com>
2017-09-23 04:59:27 -0400

    cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 extern crate csv;
-#[macro_use] extern crate lazy_static;
+#[macro_use]
+extern crate lazy_static;
 extern crate prettytable;
 extern crate regex;
 extern crate rustyline;
@@ -21,37 +22,52 @@ fn _normalize_col(col: &String) -> String {
 }
 
 fn _create_table(db: &mut sqlite3::DatabaseConnection, table_name: &str, cols: &Vec<String>) {
-    let create_columns =
-        cols.iter().map(|c| format!("{} varchar", c)).collect::<Vec<String>>().join(", ");
-    db.exec(&format!("CREATE TABLE {} ({})", table_name, create_columns)).unwrap();
+    let create_columns = cols.iter()
+        .map(|c| format!("{} varchar", c))
+        .collect::<Vec<String>>()
+        .join(", ");
+    db.exec(&format!("CREATE TABLE {} ({})", table_name, create_columns))
+        .unwrap();
 }
 
-fn _insert_row(db: &mut sqlite3::DatabaseConnection,
-               table_name: &str,
-               row: Vec<String>,
-               cols: &Vec<String>) {
+fn _insert_row(
+    db: &mut sqlite3::DatabaseConnection,
+    table_name: &str,
+    row: Vec<String>,
+    cols: &Vec<String>,
+) {
     let placeholders = cols.iter()
         .enumerate()
         .map(|(idx, _)| format!("${}", idx + 1))
         .collect::<Vec<String>>()
         .join(", ");
-    let mut stmt = db.prepare(&format!("INSERT INTO {} VALUES ({})", table_name, placeholders))
-        .unwrap();
+    let mut stmt = db.prepare(&format!(
+        "INSERT INTO {} VALUES ({})",
+        table_name,
+        placeholders
+    )).unwrap();
 
     for (idx, value) in row.iter().enumerate() {
-        stmt.bind_text((idx + 1) as sqlite3::ParamIx, value).unwrap();
+        stmt.bind_text((idx + 1) as sqlite3::ParamIx, value)
+            .unwrap();
     }
     assert!(stmt.execute().step().unwrap().is_none());
 }
 
-fn _load_table_from_path(db: &mut sqlite3::DatabaseConnection,
-                         table_name: &str,
-                         path: String)
-                         -> Vec<String> {
+fn _load_table_from_path(
+    db: &mut sqlite3::DatabaseConnection,
+    table_name: &str,
+    path: String,
+) -> Vec<String> {
     let mut num_rows = 0;
     let mut reader = csv::Reader::from_file(path).unwrap();
 
-    let normalized_cols = reader.headers().unwrap().iter().map(_normalize_col).collect();
+    let normalized_cols = reader
+        .headers()
+        .unwrap()
+        .iter()
+        .map(_normalize_col)
+        .collect();
     _create_table(db, table_name, &normalized_cols);
 
     for row in reader.decode() {
@@ -82,7 +98,9 @@ fn _print_table(conn: &mut sqlite3::DatabaseConnection, line: &str) {
     while let Some(r) = results.step().unwrap() {
         let mut row = prettytable::row::Row::new(vec![]);
         for i in 0..r.column_count() {
-            row.add_cell(prettytable::cell::Cell::new(&r.column_text(i).unwrap_or("".to_string())));
+            row.add_cell(prettytable::cell::Cell::new(
+                &r.column_text(i).unwrap_or("".to_string()),
+            ));
         }
         table.add_row(row);
     }
@@ -106,7 +124,11 @@ impl rustyline::completion::Completer for SimpleWordCompleter {
         let (start, word) =
             rustyline::completion::extract_word(line, pos, &BREAK_CHARS.iter().cloned().collect());
 
-        let matches = self.words.iter().filter(|w| w.starts_with(word)).cloned().collect();
+        let matches = self.words
+            .iter()
+            .filter(|w| w.starts_with(word))
+            .cloned()
+            .collect();
         return Ok((start, matches));
     }
 }
@@ -116,9 +138,16 @@ fn main() {
 
     let mut conn = sqlite3::DatabaseConnection::in_memory().unwrap();
 
-    let mut base_words = vec!["distinct", "select", "from", "group", "by", "order", "where",
-                              "count"]
-        .iter()
+    let mut base_words = vec![
+        "distinct",
+        "select",
+        "from",
+        "group",
+        "by",
+        "order",
+        "where",
+        "count",
+    ].iter()
         .map(|s| s.to_string())
         .collect::<Vec<String>>();
 
