74a6f9bab89255fb3cf91f0733b98290af2cc80c
Alex Gaynor <alex.gaynor@gmail.com>
2020-01-01 03:07:09 -0500

    Fix for deprecation of Error::description

--- src/main.rs
+++ src/main.rs
@@ -127,7 +127,7 @@ fn _prepare_query<'a>(
     conn: &'a mut rusqlite::Connection,
     query: &str,
 ) -> Result<rusqlite::Statement<'a>, String> {
-    conn.prepare(&query).map_err(|e| e.description().to_owned())
+    conn.prepare(&query).map_err(|e| e.to_string())
 }
 
 fn _handle_query(conn: &mut rusqlite::Connection, line: &str) -> Result<(), String> {
